e3b2808a1c3740cfb13eb607dcda2dc382ad14c3
btouellette <btouellette@2c13bf58-26fb-11df-91df-0f9350919283>
2010-03-20 18:53:54 +0000

    Template optimization code and sample bitcode file

---
+++ Phase1/OptimizeChecks.cpp
@@ -0,0 +1,39 @@
+//LLVM Bounds Checking Optimization Pass
+//CS 6241 Project Phase 1
+//Brian Ouellette
+//Chad Kersey
+//Gaurav Gupta
+
+//Run using:
+//opt -load ../../Release/lib/Phase1.so -InsertChecks -OptimizeChecks -time-passes < *.bc > /dev/null
+
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace
+{
+	struct OptimizeChecks : public FunctionPass
+	{
+		static char ID;
+		OptimizeChecks() : FunctionPass(&ID) {}
+
+		virtual bool runOnFunction(Function &F)
+		{
+			errs() << "OptimizeChecks\n";
+			//Possibly modified function so return true
+			return true;
+		}
+		
+		//Add required analyses here
+		void getAnalysisUsage(AnalysisUsage &AU) const
+		{
+			
+		}
+	};
+
+	char OptimizeChecks::ID = 0;
+	RegisterPass<OptimizeChecks> X("OptimizeChecks", "Redundant bounds checking removal");
+}
