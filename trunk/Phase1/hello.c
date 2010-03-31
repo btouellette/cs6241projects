ca7232f85b7215c05b4fee2da9ea8ae7b7caf8a6
chad.d.kersey <chad.d.kersey@2c13bf58-26fb-11df-91df-0f9350919283>
2010-03-31 22:11:32 +0000

    Playing with identify arrays from the types of their pointers. Can identify array allocations inside of functions and view their size. This is trickier than at first it may seem, because C array allocations aren't what LLVM calls "ARRAY" allocations internally.

--- trunk/Phase1/hello.c
+++ trunk/Phase1/hello.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+const char *g_s = "This is not really an array.";
+unsigned g_a[123][45];
+
 int main() {
-	int x[55];
+        unsigned i;
+        int x[55];
 	x[13]=101;
 	
 	int y[9][21];
 	y[8][20]=202;
+
+        g_a[54][40]=303;
 
 	/*int m = 4;
 	int n = 3;
