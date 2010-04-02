4c384afd8ec5d5af2a1a915d68514ebbddc940e9
chad.d.kersey <chad.d.kersey@2c13bf58-26fb-11df-91df-0f9350919283>
2010-04-02 02:10:12 +0000

    Reformatted code. 80 columns are nicer for e-mail and people like me who haven't escaped the 1970's. Figured out how to get array sizes for checks. Two things we can't do yet:
    1. Must be run after constant folding to pick up arrays that are constantly
       sized but appear in the code as though they could be dynamically sized:
    
       int m;
       int n;
       double Matrix[n][m];
    
       This isn't a big issue since constant folding takes care of it.
    
    2. Constant array expressions are already bounds checked but we can't get to
       the info needed make this fail at compile or run time. What I mean is that:
    
       static int A[15]; // Global or static array.
    
       . . .
    
       A[0] = 0;         // Access to global or static array.
    
       Is compiled into a direct store with a getElementPtrConstantExpr argument.
       The problem is that this is a private class that the optimization writer
       doesn't have easy access too. Hopefully this is accessible, because if the 
       expression is statically out-of-bounds, the getElementPtrConstantExpr object
       doesn't have the inbounds property set.

--- Phase1/InsertChecks.cpp
+++ Phase1/InsertChecks.cpp
@@ -26,51 +26,58 @@ using namespace std;
 
 namespace
 {
-        STATISTIC(numArrayAccesses, "Total number of getElementPtr instructions.");
+  STATISTIC(numArrayAccesses, "Total number of getElementPtr instructions.");
 
-	class InsertChecks : public FunctionPass
-	{
-        public:
-		static char ID;
-		InsertChecks() : FunctionPass(&ID) {}
+  class InsertChecks : public FunctionPass
+  {
+  public:
+    static char ID;
+    InsertChecks() : FunctionPass(&ID) {}
 
-		virtual bool runOnFunction(Function &F)
-		{
-			errs() << "InsertChecks\n";
+    virtual bool runOnFunction(Function &F)
+    {
+      errs() << "InsertChecks\n";
+ 
+      // Find all getElementPtr instances and add instrumentation.
+      numArrayAccesses = 0;
+      for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
 
-                        // Start by finding all of the getArrayIndex instances.
-                        numArrayAccesses = 0;
-			for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-                                GetElementPtrInst *P = dynamic_cast<GetElementPtrInst *>(&(*I));
-                                if (P == NULL) continue;
-                                const Type *T = P->getPointerOperand()->getType();
-                                const PointerType *PT = dynamic_cast<const PointerType*>(T);
-                                if (PT == NULL) continue;
-                                const ArrayType *U = dynamic_cast<const ArrayType*>(PT->getTypeAtIndex(0u));
-                                if (U == NULL) continue;
+        GetElementPtrInst *P = dynamic_cast<GetElementPtrInst *>(&(*I));
+        if (P == NULL) continue;
 
-                                errs() << *P << '\n' << *U << '\n';
-                                numArrayAccesses++;
-			}
+        const Type *T = P->getPointerOperand()->getType();
+        const PointerType *PT = dynamic_cast<const PointerType*>(T);
+        if (PT == NULL) continue;
 
-			/* Create exit condition BB
-			 * Check for getelementptr (array accesses) instructions
-			 * Either get array size from inbounds keyword or from original array def (alloca)
-			 * Add check that exits to new BB
-			 * Cleanup CFG by splitting original BB?
-			 */ 
+        const ArrayType *U = 
+          dynamic_cast<const ArrayType*>(PT->getTypeAtIndex(0u));
+        if (U == NULL) continue;
+        
+        // Get array size.
+        const uint64_t n = U->getNumElements();
+        errs() << *P << '\n' << *U << '\n' << n << " elements.\n\n";
+        numArrayAccesses++;
+      }
+
+      /* Create exit condition BB
+       * Check for getelementptr (array accesses) instructions
+       * Either get array size from inbounds keyword or from original array 
+       * def (alloca)
+       * Add check that exits to new BB
+       * Cleanup CFG by splitting original BB?
+       */ 
 			
-			//Possibly modified function so return true
-			return true;
-		}
+      //Possibly modified function so return true
+      return true;
+    }
 		
-		//Add required analyses here
-		void getAnalysisUsage(AnalysisUsage &AU) const
-		{
-			AU.setPreservesCFG();
-		}
-	};
+    //Add required analyses here
+    void getAnalysisUsage(AnalysisUsage &AU) const
+    {
+      AU.setPreservesCFG();
+    }
+  };
 
-	char InsertChecks::ID = 0;
-	RegisterPass<InsertChecks> X("InsertChecks", "Bounds checking insertion");
+  char InsertChecks::ID = 0;
+  RegisterPass<InsertChecks> X("InsertChecks", "Bounds checking insertion");
 }
