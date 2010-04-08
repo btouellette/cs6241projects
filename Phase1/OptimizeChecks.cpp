53da959714a3498520e70be17f62fa3f8a6e2098
btouellette <btouellette@2c13bf58-26fb-11df-91df-0f9350919283>
2010-04-08 01:31:16 +0000

    Implemented static checking in the optimization. Checks are removed if they can be determined at compile time.
    Also made the Makefile report stats from InsertChecks so we can see how many
    checks are in the final file.

--- Phase1/OptimizeChecks.cpp
+++ Phase1/OptimizeChecks.cpp
@@ -1,45 +1,84 @@
-//LLVM Bounds Checking Optimization Pass
-//CS 6241 Project Phase 1
-//Brian Ouellette
-//Chad Kersey
-//Gaurav Gupta
-
-//Run using:
-//opt -load ../../Release/lib/Phase1.so -InsertChecks -OptimizeChecks -time-passes < *.bc > /dev/null
+/*
+ LLVM Bounds Checking Optimization Pass
+ CS 6241 Project Phase 1
+ Brian Ouellette
+ Chad Kersey
+ Gaurav Gupta
+*/
 
 #define DEBUG_TYPE "OptimizeChecks"
 
 #define __STDC_LIMIT_MACROS
 #define __STDC_CONSTANT_MACROS
 #include <stdint.h>
+#include <set>
 
 #include "llvm/Pass.h"
 #include "llvm/Function.h"
+#include "llvm/Instructions.h"
+#include "llvm/Module.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/InstIterator.h"
 
 using namespace llvm;
+using namespace std;
 
 namespace
 {
-	struct OptimizeChecks : public FunctionPass
-	{
-		static char ID;
-		OptimizeChecks() : FunctionPass(&ID) {}
+  struct OptimizeChecks : public FunctionPass
+  {
+    static char ID;
+    OptimizeChecks() : FunctionPass(&ID) {}
+    virtual bool runOnFunction(Function &F)
+    {
+      set<Instruction*> insToDel;
+      // Iterate over all instructions in the function 
+      for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+        // Eliminate checks we can statically determine
+        // Check to see if this instruction is an upper bounds check
+        if (!I->getName().str().compare(0, 12, "_arrayref ub")) {
+          // Verify that the operand of the current instruction is a constant
+          Value *op = I->getOperand(0);
+          if (!isa<Instruction>(*op)) {
+            // Pull out the upper bound LLVM representation
+            ConstantInt *consUB = dynamic_cast<ConstantInt*>(op);
+            // Get the sign extended value (for zero extended use ZExt)
+            int64_t ub = consUB->getSExtValue();
+            
+            // Check the following index to see if it is a constant
+            op = (++I)->getOperand(0);
+            if (!isa<Instruction>(*op)) {
+              ConstantInt *consIDX = dynamic_cast<ConstantInt*>(op);
+              int64_t idx = consIDX->getSExtValue();
+              
+              // Statically check index fits in upper bound
+              if (idx <= ub) {
+                // It fits so flag instructions for deletion
+                insToDel.insert(&(*(--I)));
+                errs() << "Removed" << *I << "\n";
+                insToDel.insert(&(*(++I)));
+                errs() << "Removed" << *I << "\n";
+              }
+            }
+          }
+        }
+      }
+      
+      set<Instruction*>::iterator it;
+      for (it = insToDel.begin(); it != insToDel.end(); ++it) {
+        (*it)->removeFromParent();
+      }
+      //Possibly modified function so return true
+      return true;
+    }
+      
+    //Add required analyses here
+    void getAnalysisUsage(AnalysisUsage &AU) const
+    {
 
-		virtual bool runOnFunction(Function &F)
-		{
-			errs() << "OptimizeChecks\n";
-			//Possibly modified function so return true
-			return true;
-		}
-		
-		//Add required analyses here
-		void getAnalysisUsage(AnalysisUsage &AU) const
-		{
-			
-		}
-	};
+    }
+  };
 
-	char OptimizeChecks::ID = 0;
-	RegisterPass<OptimizeChecks> X("OptimizeChecks", "Redundant bounds checking removal");
+  char OptimizeChecks::ID = 0;
+  RegisterPass<OptimizeChecks> X("OptimizeChecks", "Redundant check removal");
 }
