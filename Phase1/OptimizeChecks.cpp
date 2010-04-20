62b9fc660d687749c3335778f76c813956cb74ef
btouellette <btouellette@2c13bf58-26fb-11df-91df-0f9350919283>
2010-04-20 14:11:04 +0000

    Reverse-merging to original

--- Phase1/OptimizeChecks.cpp
+++ Phase1/OptimizeChecks.cpp
@@ -4,7 +4,7 @@
  Brian Ouellette
  Chad Kersey
  Gaurav Gupta
- */
+*/
 
 #define DEBUG_TYPE "OptimizeChecks"
 
@@ -34,12 +34,9 @@ namespace
   {
   public:
     static char ID;
-    OptimizeChecks() : FunctionPass(&ID)
-    {
-    }
+    OptimizeChecks() : FunctionPass(&ID) {}
 
-    virtual bool
-    doInitialization(Module &M)
+    virtual bool doInitialization(Module &M) 
     {
       numChecksDeleted = 0;
     }
@@ -47,23 +44,19 @@ namespace
     virtual bool runOnFunction(Function &F)
     {
       set<Instruction*> insToDel;
-      // Iterate over all instructions in the function
-      for(Function::iterator BB = F.begin(), BB_E = F.end(); BB != BB_E; ++BB)
-      {
+      // Iterate over all instructions in the function 
+      for(Function::iterator BB = F.begin(), BB_E = F.end(); BB != BB_E; ++BB) {
         // Check for local redundant checks
         set<Instruction*> newInsToDel = eliminateRedundantLocalChecks(&(*BB));
         // Add any redundant checks to the set to be deleted later
         insToDel.insert(newInsToDel.begin(), newInsToDel.end());
-        for(BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I)
-        {
+        for(BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
           // Check to see if this instruction is an upper bounds check
-          if (!I->getName().str().compare(0, 12, "_arrayref ub"))
-          {
+          if (!I->getName().str().compare(0, 12, "_arrayref ub")) {
             // Eliminate checks we can statically determine
             Instruction *Iub = &(*I);
             Instruction *Iidx = &(*(++I));
-            if(staticallyDetermineCheck(Iub, Iidx))
-            {
+            if(staticallyDetermineCheck(Iub, Iidx)) {
               // It fits so flag instructions for deletion
               insToDel.insert(&(*(--I)));
               errs() << "Removed" << *I << "\n";
@@ -73,10 +66,9 @@ namespace
           }
         }
       }
-
+      
       set<Instruction*>::iterator it;
-      for (it = insToDel.begin(); it != insToDel.end(); ++it)
-      {
+      for (it = insToDel.begin(); it != insToDel.end(); ++it) {
         (*it)->eraseFromParent();
         numChecksDeleted++;
       }
@@ -90,31 +82,27 @@ namespace
       //errs() << "Checking" << *Iidx << "\n";
       // Verify that the operand of the current instruction is a constant
       Value *op = Iub->getOperand(0);
-      if (!isa<Instruction>(*op))
-      {
+      if (!isa<Instruction>(*op)) {
         // Pull out the upper bound LLVM representation
         ConstantInt *consUB = dynamic_cast<ConstantInt*>(op);
         // Get the sign extended value (for zero extended use ZExt)
         uint64_t ub = consUB->getSExtValue();
-
+        
         // Check the following index to see if it is a constant
         op = Iidx->getOperand(0);
-        if (!isa<Instruction>(*op))
-        {
+        if (!isa<Instruction>(*op)) {
           ConstantInt *consIDX = dynamic_cast<ConstantInt*>(op);
           uint64_t idx = consIDX->getSExtValue();
-
+          
           // Statically check index fits in upper bound
-          if (idx <= ub)
-          {
+          if (idx <= ub) {
             return true;
           }
         }
       }
       return false;
     }
-
-    //TODO: Fix: Returning a local object instead of a reference to a "new" object
+    
     set<Instruction*> eliminateRedundantLocalChecks(BasicBlock *BB)
     {
       // Set of instructions to delete which is returned to the callee
@@ -124,11 +112,9 @@ namespace
       map<Value*, Value*> checkedIns;
       map<Value*, Value*>::iterator it;
       // Iterate over every instruction in this BasicBlock
-      for(BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I)
-      {
+      for(BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
         // Only start checking when we find an upper bounds check
-        if (!I->getName().str().compare(0, 12, "_arrayref ub"))
-        {
+        if (!I->getName().str().compare(0, 12, "_arrayref ub")) {
           // Go ahead and pull out the references in the upper bounds and index
           // checks
           Value *ub = I->getOperand(0);
@@ -140,8 +126,7 @@ namespace
           bool dupeIDX = false;
           // Iterate over all the instructions we've previously added to the
           // checked set
-          for (it=checkedIns.begin(); it != checkedIns.end(); it++)
-          {
+          for (it=checkedIns.begin(); it != checkedIns.end(); it++) {
             dupeUB = dupeIDX = false;
             // Retrieves the stored upper bounds and index values (either an
             // Instruction or a ConstantInt
@@ -150,79 +135,66 @@ namespace
             // If both references are Instructions just check the pointer to see
             // if they are the same. Since the IR is SSA this check is
             // sufficient.
-            if (isa<Instruction>(*ub) && isa<Instruction>(*ub2))
-            {
-              if (ub == ub2)
-              {
+            if (isa<Instruction>(*ub) && isa<Instruction>(*ub2)) {
+              if (ub == ub2) {
                 dupeUB = true;
               }
             }
             // If they are both ConstantInts then we need to pull out the value
             // and check them against each other
-
-            else if (!isa<Instruction>(*ub) && !isa<Instruction>(*ub2))
-            {
+            else if (!isa<Instruction>(*ub) && !isa<Instruction>(*ub2)) {
               // Pull out the upper bound LLVM representation
               ConstantInt *consUB = dynamic_cast<ConstantInt*>(ub);
               // Get the sign extended value (for zero extended use ZExt)
               uint64_t ub = consUB->getSExtValue();
-
+              
               // Pull out the upper bound LLVM representation
               ConstantInt *consUB2 = dynamic_cast<ConstantInt*>(ub2);
               // Get the sign extended value (for zero extended use ZExt)
               uint64_t ub2 = consUB2->getSExtValue();
 
-              if (ub == ub2)
-              {
+              if (ub == ub2) {
                 dupeUB = true;
               }
             }
             // If both references are Instructions just check the pointer to see
             // if they are the same. Since the IR is SSA this check is
             // sufficient.
-            if (isa<Instruction>(*idx) && isa<Instruction>(*idx2))
-            {
-              if (idx == idx2)
-              {
+            if (isa<Instruction>(*idx) && isa<Instruction>(*idx2)) {
+              if (idx == idx2) {
                 dupeIDX = true;
               }
             }
             // If they are both ConstantInts then we need to pull out the value
             // and check them against each other
-
-            else if (!isa<Instruction>(*idx) && !isa<Instruction>(*idx2))
-            {
+            else if (!isa<Instruction>(*idx) && !isa<Instruction>(*idx2)) {
               // Pull out the upper bound LLVM representation
               ConstantInt *consIDX = dynamic_cast<ConstantInt*>(idx);
               // Get the sign extended value (for zero extended use ZExt)
               uint64_t idx = consIDX->getSExtValue();
-
+              
               // Pull out the upper bound LLVM representation
               ConstantInt *consIDX2 = dynamic_cast<ConstantInt*>(idx2);
               // Get the sign extended value (for zero extended use ZExt)
               uint64_t idx2 = consIDX2->getSExtValue();
 
-              if (idx == idx2)
-              {
+              if (idx == idx2) {
                 dupeIDX = true;
               }
             }
             // If the upper bounds and the index are identical to the
             // Instruction we are looking at then it is a duplicate check.
-            if (dupeUB && dupeIDX)
-            {
+            if (dupeUB && dupeIDX) {
               dupe = true;
             }
           }
           // If it isn't a dupe add it to the set of checked Instructions
-          if (!dupe)
-          {
+          if (!dupe) {
             checkedIns.insert(pair<Value*, Value*>(ub, idx));
           }
           // If it is a dupe add both checks to the list of Instructions to
           // delete
-          else
-          {
+          else {
             insToDel.insert(&(*(--I)));
             errs() << "Removed" << *I << "\n";
             insToDel.insert(&(*(++I)));
@@ -232,7 +204,7 @@ namespace
       }
       return insToDel;
     }
-
+      
     //Add required analyses here
     void getAnalysisUsage(AnalysisUsage &AU) const
     {
