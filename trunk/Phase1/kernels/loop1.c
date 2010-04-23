7f88a51ba2f0d6a1e48f9ed27ac9892cc7aa3226
chad.d.kersey <chad.d.kersey@2c13bf58-26fb-11df-91df-0f9350919283>
2010-04-23 20:18:25 +0000

    Fixed my Part 3 code. I think it works now.

--- trunk/Phase1/kernels/loop1.c
+++ trunk/Phase1/kernels/loop1.c
@@ -1,9 +1,9 @@
 int main(int argc, char** argv) {
   int n = argc*100; 
-  char A[n][n];
+  char A[100][n];
   int i, j;
 
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < 100; i++) {
     for (j = 0; j < n; j++) {
       A[i][j] = (i*j - j)%100 + (i==0?0:A[i-1][j]);
     }
