81eb02dac4fb0a947c7421e761d4b18ea7648b47
btouellette <btouellette@2c13bf58-26fb-11df-91df-0f9350919283>
2010-04-25 20:02:12 +0000

    Adding mpeg2dec benchmark.

---
+++ trunk/Phase1/benchmark/mpeg2dec/add_checks_to_objs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# A script to add bounds checks to all of the objects compiled here, because
+# for whatever reason llvm-ld removes the annotations used to create the bounds
+# checks.
+
+PASSES="-BoundChecks -OptimizeChecks -InsertChecks"
+
+for f in $@; do
+    opt -f -load ../../phase1.so $PASSES \
+    -o $f.checked -stats $f
+done
